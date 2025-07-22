9ea0ffa732cee1c0da5332d9907fa9239eecb935
Josh Thackray <thackray@rutgers.edu>
2025-07-22 12:43:04 -0400

    add see more projects link

--- source/.vitepress/components/projectList.vue
+++ source/.vitepress/components/projectList.vue
@@ -5,9 +5,43 @@
             <Project :project="project_data" />
         </template>
     </div>
+    <div class="see-more">
+        See more projects on my
+        <a href="https://github.com/quantumdot">
+            <span :class="`vpi-social-github link-icon`" :style="`--icon: url('https://api.iconify.design/simple-icons/github.svg');`"></span>
+            <span class="link-text">GitHub Profile</span>
+        </a>.
+    </div>
 </template>
 
 <script setup lang="ts">
 import Project from "./project.vue";
 import { data } from '../data/projects.data.ts';
 </script>
+
+<style scoped>
+.see-more {
+    text-align: center;
+    font-size: 14px;
+}
+.see-more a {
+    color: var(--vp-c-brand-1);
+    text-decoration: none;
+    padding-left: 5px;
+}
+.see-more a:hover {
+    text-decoration: underline;
+}
+
+.link-icon {
+    display: inline-block;
+    width: 18px;
+    height: 18px;
+    vertical-align: middle;
+    padding-right:10px;
+}
+.link-text {
+    padding-left: 8px;
+    vertical-align: middle;
+}
+</style>
